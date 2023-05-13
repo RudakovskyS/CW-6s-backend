1c0707b4cee51930961122284382a8c322a965ba
RudakovskyS <rudakovsky_sd@mail.ru>
2023-05-13 15:09:07 +0300

    rewrite logic of rating to remove rates and be usable at all

--- src/rate/rate.service.ts
+++ src/rate/rate.service.ts
@@ -44,6 +44,36 @@ export class RateService {
                     userUser_id: user.user_id
                 }
             })
+        } else if (await this.checkIfPostLikedByUser(+id, +user.user_id)){
+            const like = await this.prisma.like.findFirst({
+                where: {
+                    userUser_id: +user.user_id,
+                    postPost_id: +id
+                }
+            })
+            return this.prisma.like.delete({
+                where: {
+                    like_id: like.like_id
+                }
+            })
+        } else if (await this.checkIfPostDislikedByUser(+id, +user.user_id)) {
+            const dislike = await this.prisma.dislike.findFirst({
+                where: {
+                    userUser_id: +user.user_id,
+                    postPost_id: +id
+                }
+            })
+            await this.prisma.dislike.delete({
+                where: {
+                    dislike_id: dislike.dislike_id
+                }
+            })
+            return this.prisma.like.create({
+                data: {
+                    postPost_id: +id,
+                    userUser_id: user.user_id
+                }
+            })
         }
         return
     }
@@ -51,6 +81,36 @@ export class RateService {
     async dislikePost(id: number, user: any) {
         user = JSON.parse(user);
         if (await this.checkIfPostLikedByUser(id, user.user_id) == 0 && await this.checkIfPostDislikedByUser(id, user.user_id) == 0) {
+            return this.prisma.dislike.create({
+                data: {
+                    postPost_id: +id,
+                    userUser_id: user.user_id
+                }
+            })
+        }else if (await this.checkIfPostDislikedByUser(+id, +user.user_id)){
+            const dislike = await this.prisma.dislike.findFirst({
+                where: {
+                    userUser_id: +user.user_id,
+                    postPost_id: +id
+                }
+            })
+            return this.prisma.dislike.delete({
+                where: {
+                    dislike_id: dislike.dislike_id
+                }
+            })
+        } else if (await this.checkIfPostLikedByUser(+id, +user.user_id)) {
+            const like = await this.prisma.like.findFirst({
+                where: {
+                    userUser_id: +user.user_id,
+                    postPost_id: +id
+                }
+            })
+            await this.prisma.like.delete({
+                where: {
+                    like_id: like.like_id
+                }
+            })
             return this.prisma.dislike.create({
                 data: {
                     postPost_id: +id,
